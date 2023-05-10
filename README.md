be3588ae14288975311312769f84d45df4bb7125
ryo2357 <66926018+ryo2357@users.noreply.github.com>
2023-05-10 09:40:54 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Raspberry Pi Pico Rust 開発用テンプレート
+
+[rp-rs/rp2040-project-template](https://github.com/rp-rs/rp2040-project-template)を自分が使いやすいように調整
+
+`cargo generate --git https://github.com/ryo2357/rp2040-template --name [プロジェクト名]`
---
+++ src/main.rs
@@ -0,0 +1,72 @@
+#![no_std]
+#![no_main]
+
+use bsp::entry;
+
+// デバッガのクレート。デバッグピンと通信する準備は必要
+use defmt::*;
+use defmt_rtt as _;
+
+use embedded_hal::digital::v2::{InputPin, OutputPin};
+use panic_probe as _;
+
+use rp_pico as bsp;
+
+use bsp::hal::{
+    clocks::{init_clocks_and_plls, Clock},
+    pac,
+    sio::Sio,
+    watchdog::Watchdog,
+};
+
+#[entry]
+fn main() -> ! {
+    info!("Program start");
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+    let sio = Sio::new(pac.SIO);
+
+    // External high-speed crystal on the pico board is 12Mhz
+    let external_xtal_freq_hz = 12_000_000u32;
+    let clocks = init_clocks_and_plls(
+        external_xtal_freq_hz,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    let pins = bsp::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let mut led_pin = pins.led.into_push_pull_output();
+    let btn = pins.gpio14.into_pull_up_input();
+
+    loop {
+        if btn.is_low().unwrap() {
+            led_pin.set_high().unwrap();
+        } else {
+            led_pin.set_low().unwrap();
+        }
+    }
+
+    // loop {
+    //     info!("on!");
+    //     led_pin.set_high().unwrap();
+    //     delay.delay_ms(500);
+    //     info!("off!");
+    //     led_pin.set_low().unwrap();
+    //     delay.delay_ms(500);
+    // }
+}
